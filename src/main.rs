860159944acf18e5ee58bdaee1c19cb82219aa41
Tommi Hyppänen <thyppane@gmail.com>
2024-02-11 00:15:25 +0200

    Some SSD1306 messaging

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,58 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
+use embassy_stm32::dma::NoDma;
 use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::i2c::{Error, I2c};
+use embassy_stm32::time::Hertz;
+use embassy_stm32::{bind_interrupts, i2c, peripherals};
 use embassy_time::Timer;
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyle},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    text::Text,
+};
+use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
 use {defmt_rtt as _, panic_probe as _};
+const ADDRESS: u8 = 0x5F;
+const WHOAMI: u8 = 0x0F;
+
+bind_interrupts!(struct Irqs {
+    I2C1_EV => i2c::EventInterruptHandler<peripherals::I2C1>;
+    I2C1_ER => i2c::ErrorInterruptHandler<peripherals::I2C1>;
+});
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
-    info!("Hello World!");
+    info!("Program started.");
 
     let mut led = Output::new(p.PC13, Level::High, Speed::Low);
+
+    let mut i2c = I2c::new(
+        p.I2C1,
+        p.PB6,
+        p.PB7,
+        Irqs,
+        NoDma,
+        NoDma,
+        Hertz(100_000),
+        Default::default(),
+    );
+
+    let interface = I2CDisplayInterface::new(i2c);
+    let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate180)
+        .into_buffered_graphics_mode();
+    display.init().unwrap();
+
+    // Create a new character style
+    let style = MonoTextStyle::new(&FONT_6X10, BinaryColor::On);
+
+    // Create a text at position (20, 30) and draw it using the previously defined style
+    Text::new("Hello Rust!", Point::new(20, 30), style).draw(&mut display).unwrap();
+
+    display.flush().unwrap();
 
     loop {
         info!("high");
@@ -21,6 +63,7 @@ async fn main(_spawner: Spawner) {
 
         info!("low");
         led.set_low();
+        // Timer::after_millis(300).await;
         Timer::after_micros(300_000).await;
     }
 }
